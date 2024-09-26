3ca1414891fc02ab8a9b26683ea6bca79e944fcc
Vaibhav Kokare <vaibhavkokare321@gmail.com>
2024-09-26 11:23:44 +0530

    Created authentication function Added starter icons Added model selection and settings

---
+++ app.py
@@ -0,0 +1,207 @@
+import chainlit as cl
+from chainlit.input_widget import Select, Switch, Slider
+from typing import Optional
+
+import torch
+
+from langchain_community.document_loaders import PyPDFLoader
+
+from langchain_huggingface import HuggingFaceEmbeddings
+from sentence_transformers import SentenceTransformer
+
+from pinecone import Pinecone, ServerlessSpec
+from langchain_pinecone import PineconeVectorStore
+import pinecone
+
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+PINECONE_API_KEY = os.environ.get("PINECONE_API_KEY")
+GROQ_API_KEY = os.environ.get("GROQ_API_KEY")
+
+
+# if torch.cuda.is_available():
+#     device = torch.device("cuda")
+#     print("GPU is available")
+# else:
+#     device = torch.device("cpu")
+#     print("GPU is not available, using CPU")
+
+
+
+embedding_model_1 = HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")
+
+pc = Pinecone(api_key=PINECONE_API_KEY)
+
+index_name = "book-sync"
+if index_name not in pc.list_indexes().names():
+    pc.create_index(
+        name=index_name,
+        dimension=384,
+        metric="cosine",
+        spec=ServerlessSpec(
+            cloud='aws',
+            region='us-east-1'
+        )
+    )
+    print("Created index...\n", pc.describe_index(index_name))
+else:
+    print("Index already exists")
+
+index = pc.Index(index_name)
+
+docsearch_instance = PineconeVectorStore(
+    pinecone_api_key=PINECONE_API_KEY,
+    embedding=embedding_model_1,
+    index=index,
+)
+
+
+def get_similar_doc(query, k=3, score=True):
+    if score:
+        similar_docs = docsearch_instance.similarity_search_with_score(query, k=k)
+    else:
+        similar_docs = docsearch_instance.similarity_search(query, k=k)
+    return similar_docs
+
+
+# Simulated user storage (replace with a database in a real application)
+users_db = {
+    "vaibhav": {"password": "123", "role": "ADMIN"},
+}
+
+@cl.password_auth_callback
+def auth_callback(username: str, password: str) -> Optional[cl.User]:
+    print(username, password)
+    user = users_db.get(username)
+    if user and user["password"] == password:
+        return cl.User(identifier=username, metadata={"role": user["role"]})
+    return None
+
+
+@cl.set_starters
+async def set_starters():
+    return [
+        cl.Starter(
+            label="Summarize Books",
+            message="Help me summarize a refrence book",
+            icon="./public/scholar_icon.png",
+        ),
+
+        cl.Starter(
+            label="Data Scientist",
+            message="Research topic ___",
+            icon="./public/idea_icon.png",
+        ),
+
+        cl.Starter(
+            label="Crawler",
+            message="Crawl for ___",
+            icon="./public/icons8-flame-64.png",
+        ),
+    ]
+
+
+@cl.on_chat_start
+async def on_chat_start():
+    settings = await cl.ChatSettings(
+        [
+            Select(
+                id="Model",
+                label="Model",
+                values=[
+                    "mixtral-8x7b-32768", 
+                    "llama3-70b-8192", 
+                    "llama3-8b-8192"
+                ],
+                initial_index=0,
+            ),
+            Switch(
+                id="Streaming", 
+                label="Streaming", 
+                initial=True
+            ),
+            Slider(
+                id="Temperature",
+                label="Temperature",
+                initial=1,
+                min=0,
+                max=2,
+                step=0.1,
+            ),
+        ]
+    ).send()
+
+
+@cl.on_settings_update
+async def settings_update(settings):
+    print(settings)
+
+
+@cl.on_message
+async def on_message(message: cl.Message):
+    if message.content == "Help me summarize a refrence book":        
+        files = await cl.AskFileMessage(
+            content= "Please upload a file!",
+            accept= ["text/plain", "application/pdf"],
+            max_files=2,
+            max_size_mb=100,
+            timeout=60
+        ).send()
+
+        print(files)
+
+        await cl.Message(
+            content=f"{files[0].name} is successfully uploaded"
+        ).send()
+        
+        loader = PyPDFLoader(files[0].path)
+        pages = loader.load_and_split()
+        
+        # Create instance -> embed doc and upsert
+        PineconeVectorStore.from_documents(
+            pinecone_api_key=PINECONE_API_KEY,
+            index_name=index_name,
+            embedding=embedding_model_1,
+            documents=pages,
+        )
+
+        print("Uploading doc done!")
+        return None
+    
+    elif message.content == "Crawl for ___":
+        elements = [
+            cl.Pdf(
+                name="pdf1", 
+                display="inline", 
+                path="D:/BSC-CS Vaibhav/SEMISTER V/Linux Server Administration/ubuntu-server-guide-2022-08-03.pdf"
+            )
+        ]
+
+        cl.Message(
+            content="Look at this local pdf!", 
+            elements=elements
+        ).send()
+
+
+
+    similar_docs = get_similar_doc(message.content, k=1)
+    await cl.Message(
+        content=similar_docs
+    ).send()
+
+
+@cl.on_stop
+async def on_stop():
+    await cl.Message(
+        content="Chat Interrupted!"
+    ).send()
+    
+
+
+@cl.on_chat_end
+async def on_chat_end():
+    await cl.Message(
+        content="Thank You!"
+    ).send()
